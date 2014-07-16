9fe816e0b06ee155b6b7f502a689cf3f1ccfc70a
JT Archie <jtarchie@gmail.com>
2014-07-15 23:31:57 -0400

    binding with changing values

--- spec/javascripts/features/binding_spec.js
+++ spec/javascripts/features/binding_spec.js
@@ -8,17 +8,42 @@ describe('Binding values to a template from the scope', function() {
   });
 
   describe('when a template reference a scope value', function() {
+    var age, scope;
     beforeEach(function() {
       app.$route({
         url: '/',
         template: 'My age is <span tg-bind="age"></span>',
-        controller: function($scope) { $scope.age = 31; }
+        controller: function($scope) {
+          scope = $scope;
+          $scope.age = 31;
+        }
       });
       app.$go();
     });
 
     it('should should evaluate in to the DOM', function() {
       expect(root.textContent).toEqual('My age is 31');
+    });
+
+    describe('when the value on the scope changes', function() {
+      beforeEach(function() {
+        scope.$apply(function() {
+          scope.age = 100;
+        });
+      });
+
+      it('changes the value in the template', function() {
+        expect(root.textContent).toEqual('My age is 100');
+      });
+    });
+  });
+
+  pending('when the scope value', function() {
+    describe('is a function', function() {
+    });
+    describe('is a collection', function() {
+    });
+    describe('is a string', function() {
     });
   });
 });
